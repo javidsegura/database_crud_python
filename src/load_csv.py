a68d5afd7f78c8deb501c6f16e153567c0782dc6
Javi D. Segura <129964070+javidsegura@users.noreply.github.com>
2024-04-14 21:16:11 +0200

    Update load_csv.py

--- src/load_csv.py
+++ src/load_csv.py
@@ -4,10 +4,18 @@ import pandas as pd
 
 # SETTING DATASET 
 
-url = "/Users/javierdominguezsegura/Programming/Python/Side projects/Database Management System/data/MUSIC - DATASET - Sheet1 (1).csv"
+# url = "/Users/.../data/MUSIC - DATASET - Sheet1 (1).csv"
+
+url = "/Users/.../data/onlinefoods.csv"
 
 def creating_datframe(url = url):
       global df
       df = pd.read_csv(url)
-      # df = df.drop(["spotify_url", "youtube_url"], axis = 1) # Removing URLs
 
+      # Get the header to be in lowercase; if already in lower case do nothing
+      df.columns = [col.lower() if col != col.lower() else col for col in df.columns]
+
+      with open(url, "w") as file:
+            df.to_csv(file, header=True,index = False)
+      
+      # df = df.drop(["spotify_url", "youtube_url"], axis = 1) # Removing URLs
